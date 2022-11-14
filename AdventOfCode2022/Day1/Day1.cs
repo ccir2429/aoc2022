f80dcb143ca9dd356d7c43d49c29159d8d5d8efd
cosma.cosmin <cosma.cosmin@PC310767>
2022-11-14 12:46:43 +0200

    aoc day1 template c#

---
+++ AdventOfCode2022/Day1/Day1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode2022
+{
+    public static class Day1
+    {
+        public static void SolveFirstChallange()
+        {
+            Console.WriteLine("Ho Ho Ho!");
+        }
+    }
+}
---
+++ AdventOfCode2022/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdventOfCode2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Day1.SolveFirstChallange();
+        }
+    }
+}
